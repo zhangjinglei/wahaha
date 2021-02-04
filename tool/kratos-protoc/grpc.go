db860aa2528353201684cbd41a52a7ed4c0c836f
Administrator <admin@example.com>
2021-02-04 13:50:38 +0800

    zjl

--- tool/kratos-protoc/grpc.go
+++ tool/kratos-protoc/grpc.go
@@ -4,23 +4,23 @@ import "os/exec"
 
 const (
 	//
-	//_getGRPCGen = "go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_getGRPCGen = "go get -u github.com/gogo/protobuf/protoc-gen-gofast"
-	//_grpcProtoc = `protoc --proto_path=%s --proto_path=%s --proto_path=%s --go_out=. --go-grpc_out=.`
-	_grpcProtoc = `protoc --proto_path=%s --proto_path=%s --proto_path=%s --gofast_out=plugins=grpc:.`
+	_getGRPCGen = "go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	//_getGRPCGen = "go get -u github.com/gogo/protobuf/protoc-gen-gofast"
+	_grpcProtoc = `protoc --proto_path=%s --proto_path=%s --proto_path=%s --go_out=. --go-grpc_out=.`
+	//_grpcProtoc = `protoc --proto_path=%s --proto_path=%s --proto_path=%s --gofast_out=plugins=grpc:.`
 )
 
 func installGRPCGen() error {
-	if _, err := exec.LookPath("protoc-gen-gofast"); err != nil {
-		if err := goget(_getGRPCGen); err != nil {
-			return err
-		}
-	}
-	//if _, err := exec.LookPath("protoc-gen-go-grpc"); err != nil {
+	//if _, err := exec.LookPath("protoc-gen-gofast"); err != nil {
 	//	if err := goget(_getGRPCGen); err != nil {
 	//		return err
 	//	}
 	//}
+	if _, err := exec.LookPath("protoc-gen-go-grpc"); err != nil {
+		if err := goget(_getGRPCGen); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
